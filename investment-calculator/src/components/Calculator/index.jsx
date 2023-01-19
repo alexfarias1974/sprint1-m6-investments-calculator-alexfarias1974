51bda5c978ada8ba952c01be3b4a5ddd6ac85af5
alexfarias1974 <alex_wfariaas@yahoo.com.br>
2023-01-18 22:52:50 -0400

    feat: challenge concluded without extras

--- investment-calculator/src/components/Calculator/index.jsx
+++
@@ -1,9 +0,0 @@
-// import './style.css'
-
-// const Calculator = () => {
-//     return <div className="container">
-//         <h1>Teste</h1>
-//     </div>
-// }
-
-// export default Calculator
--- investment-calculator/src/pages/Home/index.jsx
+++ investment-calculator/src/pages/Home/index.jsx
@@ -1,42 +1,90 @@
-// import Calculator from "./components/Calculator";
 import {useForm} from "react-hook-form";
-
+import axios from "axios";
+import { useMutation } from "react-query";
+import "./style.css"
 
 const Home = () => {
-    const {register} = useForm();
+    const {register, handleSubmit} = useForm();
+    
+    const {isLoading, isError, data, mutate} = useMutation((dataForm) => {
+        return axios.post("https://frontend-challenge-7bu3nxh76a-uc.a.run.app/", dataForm)
+        .then(({data}) => data)
+        
+    })
+
+    if (isLoading) {
+        return <div className="loader">
+            <span>Loading...</span>
+        </div>;
+    }
+
+    if (isError) {
+        return (
+            <div>
+                <h2 className='error'>Algo deu errado!!!</h2>
+            </div>
+    )}
+
+    const onSubmit = (data) => {
+        mutate(data)
+    }   
+
     return (
         <div className="App">
             <div className="container">
-                <div className="formSimulator">
+                <div className="simulator">
                     <h2>Simule sua Antecipação</h2>
-                    <form>
-                        <label htmlFor="saleValue">Informe o valor da venda *</label>
-                        <input type="number" id="saleValue" {... register("saleValue")} />
+                    <form className="formSimulator">
+                        <label className="label" htmlFor="amount">Informe o valor da venda *</label>
+                        <input className="input" type="number" min="0.00" step="0.01" id="amount" {... register("amount")} />
 
-                        <label htmlFor="installments">Em quantas parcelas *</label>
-                        <input type="number" id="installments" {... register("installments")} />
+                        <label className="label" htmlFor="installments">Em quantas parcelas *</label>
+                        <input className="input" type="number" id="installments" {... register("installments")} />
 
                         <p>Máximo de 12 parcelas</p>
 
-                        <label htmlFor="mdrTax">Em quantas parcelas *</label>
-                        <input type="number" id="mdrTax" {... register("mdrTax")} />
+                        <label className="label" htmlFor="mdr">Informe o percentual de MDR *</label>
+                        <input className="input" type="number" id="mdr" {... register("mdr")} />
                     </form>
+                    <div className="divButton">
+                        <button className="grow_spin" type="submit" onClick={handleSubmit(onSubmit)}>Enviar</button>
+                    </div>
                 </div>
-                    <h3><i>VOCÊ RECEBERÁ:</i></h3>
-                    <ul>
-                        <li><i>Amanhã</i></li>
-                        <li><i>Em 15 dias:</i></li>
-                        <li><i>Em 30 dias:</i></li>
-                        <li><i>Em 90 dias:</i></li>
-                    </ul>
-                    <ul>
-                        <li><i>R$ 0,00</i></li>
-                        <li><i>R$ 0,00</i></li>
-                        <li><i>R$ 0,00</i></li>
-                        <li><i>R$ 0,00</i></li>
-                    </ul>
-                <div className="resultSimulator"></div>
-                {/* <Calculator /> */}
+                
+                <div className="resultSimulator">
+                    <div className="titleData">
+                        <h3><i>VOCÊ RECEBERÁ:</i></h3>
+                    </div>
+                    <div className="dataSimulator">
+                        <ul className="ulDaysAntecipation">
+                            <li><i>Amanhã:</i></li>
+                            <li><i>Em 15 dias:</i></li>
+                            <li><i>Em 30 dias:</i></li>
+                            <li><i>Em 90 dias:</i></li>
+                        </ul>
+                        <ul className="ulValuesAntecipation">
+                            {!data ? (
+                                <>
+                                <li><i>R$ 0,00</i></li>
+                                <li><i>R$ 0,00</i></li>
+                                <li><i>R$ 0,00</i></li>
+                                <li><i>R$ 0,00</i></li>
+                                </>
+                            )
+                        : 
+                        (
+                            <>
+                            <li><i>{data["1"].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</i></li>
+                            <li><i>{data["15"].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</i></li>
+                            <li><i>{data["30"].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</i></li>
+                            <li><i>{data["90"].toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}</i></li>
+                            </>
+                        )
+                        } 
+                            
+                        </ul>
+                    </div>
+                </div>    
             </div>
         </div>
     )
